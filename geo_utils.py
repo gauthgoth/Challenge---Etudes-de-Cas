7ee34070af64010703d5fd884a09a532c3de636d
romain fournier <=>
2023-01-18 01:44:24 +0100

    added first viz for bicycle traffic in Paris

--- geo_utils.py
+++ geo_utils.py
@@ -1,6 +1,8 @@
 import requests
 import numpy as np
 import json
+from requests.structures import CaseInsensitiveDict
+import pandas as pd
 
 def filter_france_metr(df):
     mask = df['dep'].apply(lambda x : len(x)<3)
@@ -20,3 +22,29 @@ def generate_mapping_insee_to_coords(insee_codes):
     with open('communes_coordinates.json','w') as json_file:
         json.dump(mapping_insee_codes_coord,json_file)
         json_file.close()
+
+def get_postcode_from_coord(lon,lat):
+    url = "https://api.geoapify.com/v1/geocode/reverse?lat={}&lon={}&apiKey=6642b907a72940dcb7b6b3e0f9a1605a".format(lat,lon)
+
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+
+    resp = requests.get(url, headers=headers)
+    try :
+        postcode = resp.json()['features'][0]['properties']['postcode']
+    except:
+        return None
+    return postcode
+
+def generate_postcodes(df_comptage):
+    piv_table=df_comptage.pivot_table(index='id_compteur', values='coordinates', aggfunc='head')
+    piv_table=piv_table['coordinates'].str.split(',',expand=True)
+    piv_table=piv_table.applymap(float)
+    postal_codes = piv_table.apply(lambda x : get_postcode_from_coord(x[1],x[0]) , axis=1)
+    postal_codes=pd.DataFrame(postal_codes, columns=['postal_code'])
+    localisations=df_comptage.merge(postal_codes,left_index=True, right_index=True)
+    localisations=localisations[['id_compteur','nom_compteur','coordinates','postal_code']]
+    return localisations
+
+def filter_paris_postcodes(df):
+    return df[df['postal_code'].astype(str).str[:2]=='75']
